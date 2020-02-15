275aac90bfbd04a20f82a6f0f65c8ee364b2b817
Calvin Brownlee <calvinbrownlee@gmail.com>
2020-02-15 14:50:53 -0500

    feat: Create grid.cpp an example of a 2d vector

---
+++ grid/grid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+
+
+int main() {
+  // TODO: Declare a "board" variable here, and store
+  // the data provided above.
+   vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 0, 0, 0, 1, 0}};
+
+    // For loop to iterate through vector
+  // for(int i : board)
+  //   cout << i << " ";
+  // cout << "\n";
+
+  cout << "Hello!" << "\n";
+}
